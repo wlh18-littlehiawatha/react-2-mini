de5937ce34c560cf1d99c47819dcd7b03ebb96f9
Jacob Hall <halljaco@yahoo.com>
2020-03-25 12:56:14 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -9,25 +9,72 @@ import TextContainer from './components/TextContainer';
 
 class App extends Component {
   // constructor
+  constructor(props){
+    super(props)
+      this.state = {
+        fontColor: 'black',
+        fontSize: 12,
+        fontFamily: 'monospace',
+        allowEdit:'true'
+      };
+
+      this.updateColor = this.updateColor.bind(this)
+      this.updateSize = this.updateSize.bind(this)
+      this.updateFamily = this.updateFamily.bind(this)
+      this.updateStatus = this.updateStatus.bind(this)
+  }
 
   // updateColor
-
+updateColor(val){
+  this.setState({
+    fontColor: val
+  })
+}
   // updateSize
-
+  updateSize(val){
+    this.setState({
+      fontSize: val
+    })
+  }
   // updateFamily
-
+  updateFamily(val){
+    this.setState({
+      fontFamily: val
+    })
+  }
   // updateEditStatus
+  updateStatus(val){
+    this.setState({
+      fontStatus: val
+    })
+  }
+
 
   render() {
     return (
       <div>
         <div className="headerBar">
-          {/* Render EditToggle */}
-          {/* Render ColorChanger */}
-          {/* Render SizeChanger */}
-          {/* Render FamilyChanger */}
+          <EditToggle update={this.updateEditStatus} />
+          <ColorChanger
+            update={this.updateColor}
+            allowEdit={this.state.allowEdit}
+          />
+          <SizeChanger
+            update={this.updateSize}
+            allowEdit={this.state.allowEdit}
+          />
+          <FamilyChanger
+            update={this.updateFamily}
+            allowEdit={this.state.allowEdit}
+          />
         </div>
-        <div className="textArea">{/* Render TextContainer */}</div>
+        <div className="textArea">
+          <TextContainer
+            fontFamily={this.state.fontFamily}
+            fontColor={this.state.fontColor}
+            fontSize={this.state.fontSize}
+          />
+        </div>
       </div>
     );
   }
--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -6,6 +6,8 @@ export default class TextContainer extends Component {
     this.state = {
       text: '',
     };
+
+    this.updateText = this.updateText.bind(this)
   }
 
   updateText(e) {
@@ -18,7 +20,10 @@ export default class TextContainer extends Component {
     return (
       <div className="textContainer">
         <textarea
-          // style={  }
+          style={ {color = this.props.fontColor, 
+                  fontSize = this.props.fontSize,
+                  fontFamily = this.props.fontFamily
+          }}
           onChange={this.updateText}
           value={this.state.text}
           placeholder="Start typing your thoughts here!"
